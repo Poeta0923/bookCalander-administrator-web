b2352f2448b16150c0d271da734edd06c89f3df1
Kwangsoo Kim <gimgwangsu0@gmail.com>
2025-04-11 21:00:47 +0900

    add paging function at /member/search/:currentPage

--- router/memberRouter.js
+++ router/memberRouter.js
@@ -14,7 +14,7 @@ router.get('/delete/:memberId', (req, res)=>{
 })
 
 //회원 검색란에 정보 기입 후 검색버튼 클릭 시 요청 라우팅
-router.post('/search', (req, res)=>{
+router.post('/search/:currentPage', (req, res)=>{
     member.search(req, res);
 })
 
